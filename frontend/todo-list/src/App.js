31dd95a2b442f8941658d374413940010778cec1
Shawn Yang <yangshawn14@gmail.com>
2025-01-26 19:39:58 -0700

    Fix routes

--- frontend/todo-list/src/App.js
+++ frontend/todo-list/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
@@ -7,18 +7,21 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/tasks" element={<TaskList />} />
-          <Route path="/updateTask/:id" element={<UpdateTask />} />
-        </Routes>
-      </main>
-      <Footer />
-
-    </div>
+    <Router>  {/* Wrap the routes with Router */}
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            {/* Define routes */}
+            <Route path="/" element={<TaskList />} /> {/* Default route */}
+            <Route path="/tasks" element={<TaskList />} />
+            <Route path="/updateTask/:id" element={<UpdateTask />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
